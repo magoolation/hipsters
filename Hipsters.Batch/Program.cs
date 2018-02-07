e96d6e102b3f50233d3b511af7884a6474ad9405
Alexandre Costa <magoolation@me.com>
2018-02-07 14:55:56 -0200

    Added the solution

---
+++ Hipsters.Batch/Program.cs
@@ -0,0 +1,25 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hipsters.Batch
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var document = await BrowsingContext.New(config).OpenAsync(@"http://www.hipsters.tech");
+            foreach(var element in document.QuerySelectorAll("h1"))
+            {
+                Console.WriteLine(element.˜˜˜˜˜TextContent);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
